6251a50aba8f0a4de867cae80278a764b2ad0ce7
Captain <Samsonkel1234@gmail.com>
2026-01-22 12:11:01 +0100

    tranferred some necessary programs to resusable function to promote clean structure. 50% donw with the filtered items that wasn't showing the modal when click even is applied.

--- my-recipe-app/recipe-app.js
+++ my-recipe-app/recipe-app.js
@@ -1,5 +1,5 @@
 const RecipeList = document.querySelector("#recipe-list");
-let RecipePackage = document.querySelector("#recipe-package");
+const RecipePackage = document.querySelector("#recipe-package");
 const input = document.querySelector("#filter-field");
 
 function handleRecipeList() {
@@ -89,21 +89,109 @@ function handleRecipeList() {
         "Add-ins",
       ],
     },
+    {
+      image: "",
+      name: "Cajun-sausage",
+      price: 70,
+      // id: 5,
+      ingredient: [
+        "Smoked sausage",
+        "Rice",
+        "Bell Peppers",
+        "Onions",
+        "Garlic",
+        "Cajun Seasoning",
+        "Chicken Broth",
+        "Olive Oil",
+        "Add-ins",
+      ],
+    },
+    {
+      image: "",
+      name: "Cajun-sausage",
+      price: 70,
+      // id: 5,
+      ingredient: [
+        "Smoked sausage",
+        "Rice",
+        "Bell Peppers",
+        "Onions",
+        "Garlic",
+        "Cajun Seasoning",
+        "Chicken Broth",
+        "Olive Oil",
+        "Add-ins",
+      ],
+    },
+    {
+      image: "",
+      name: "Cajun-sausage",
+      price: 70,
+      // id: 5,
+      ingredient: [
+        "Smoked sausage",
+        "Rice",
+        "Bell Peppers",
+        "Onions",
+        "Garlic",
+        "Cajun Seasoning",
+        "Chicken Broth",
+        "Olive Oil",
+        "Add-ins",
+      ],
+    },
+    {
+      image: "",
+      name: "Cajun-sausage",
+      price: 70,
+      // id: 5,
+      ingredient: [
+        "Smoked sausage",
+        "Rice",
+        "Bell Peppers",
+        "Onions",
+        "Garlic",
+        "Cajun Seasoning",
+        "Chicken Broth",
+        "Olive Oil",
+        "Add-ins",
+      ],
+    },
+    {
+      image: "",
+      name: "Taco",
+      price: 70,
+      // id: 5,
+      ingredient: [
+        "Smoked sausage",
+        "Rice",
+        "Bell Peppers",
+        "Onions",
+        "Garlic",
+        "Cajun Seasoning",
+        "Chicken Broth",
+        "Olive Oil",
+        "Add-ins",
+      ],
+    },
   ];
 
-  let result = Receipes.map((recipe) => {
-    return `
-          <div id="item" class="recipe-container">
-                      <div class="recipe-img">
-                          <span>${recipe.name}</span>
-                      </div>
-                      <h3>price:<span>$${recipe.price}</span></h3>
-                      <p>${recipe.ingredient.slice(0, 5)}....</p>
-                  </div>
-         `;
-  });
+  let result = Receipes.map(Template);
   RecipePackage.innerHTML = result.join(" ");
-  // let filterResult;
+
+  const items = RecipePackage.querySelectorAll("#item");
+  items.forEach((item, index) => {
+    item.addEventListener("click", (e) => {
+      e.stopPropagation();
+      const selectedRecipe = Receipes[index];
+      const modal = document.querySelector(".modal");
+      if (modal) {
+        modal.remove();
+      }
+      openModal(selectedRecipe);
+    });
+  });
+  let filterResult;
   input.addEventListener("input", () => {
     if (
       input.value.trim().toLowerCase() === "" ||
@@ -113,45 +201,39 @@ function handleRecipeList() {
     } else {
       filterResult = Receipes.filter(
         (recipe) =>
-          // const searchItem = input.value.trim().toLowerCase();
           recipe.name.toLocaleLowerCase() === input.value.trim().toLowerCase(),
-      ).map((recipe) => {
-        return `<div id="item" class="recipe-container">
+      ).map(Template);
+    }
+
+    RecipePackage.innerHTML = filterResult.join(" ");
+  });
+  RecipePackage.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const target = e.target;
+    console.log(e);
+    const modal = document.querySelector(".modal");
+    if (modal) {
+      modal.remove();
+    }
+    if ((target.id = "item")) {
+      openModal(target);
+    }
+  });
+}
+
+window.addEventListener("DOMContentLoaded", handleRecipeList);
+
+function Template(recipe) {
+  return `
+          <div id="item" class="recipe-container">
                       <div class="recipe-img">
                           <span>${recipe.name}</span>
                       </div>
                       <h3>price:<span>$${recipe.price}</span></h3>
                       <p>${recipe.ingredient.slice(0, 5)}....</p>
-                  </div>`;
-      });
-    }
-
-    RecipePackage.innerHTML = filterResult.join(" ");
-
-    console.log(filterResult);
-  });
-  // result.addEventListener("click", (e) => {
-  //   if (e.target) {
-  //     openModal;
-  //   }
-  // item});
-  const items = RecipePackage.querySelectorAll("#item");
-  items.forEach((item, index) => {
-    item.addEventListener("click", (e) => {
-      e.stopPropagation();
-      const selectedRecipe = Receipes[index];
-      // selectedRecipe.();
-      const modal = document.querySelector(".modal");
-      console.log(modal);
-      if (modal) {
-        modal.remove();
-      }
-      openModal(selectedRecipe);
-    });
-  });
+                  </div>
+         `;
 }
-
-window.addEventListener("DOMContentLoaded", handleRecipeList);
 
 function openModal(recipe) {
   const modal = document.createElement("div");
@@ -175,8 +257,6 @@ function openModal(recipe) {
   // Inside your openModal function
   const purchaseBtn = modal.querySelector(".purchase");
   const confirmCheck = modal.querySelector("#checkbox");
-  console.log(confirmCheck);
-  console.log(confirmCheck.checked);
 
   confirmCheck.addEventListener("change", () => {
     purchaseBtn.disabled = !confirmCheck.checked;
